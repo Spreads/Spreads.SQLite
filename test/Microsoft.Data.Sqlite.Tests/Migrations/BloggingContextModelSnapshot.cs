80f134617cae672c26b7b162aa8d0c3b2bc7ec4c
Victor Baybekov <vbaybekov@gmail.com>
2017-02-03 09:45:27 +0300

    Migrations and EF7 work (almost), but so unfriendly/unstable and so many gotchas

---
+++ test/Microsoft.Data.Sqlite.Tests/Migrations/BloggingContextModelSnapshot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Data.Sqlite.Tests.EntityFramework;
+
+namespace Microsoft.Data.Sqlite.Tests.Migrations
+{
+    [DbContext(typeof(BasicEFTests.BloggingContext))]
+    partial class BloggingContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("Microsoft.Data.Sqlite.Tests.EntityFramework.BasicEFTests+Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("Microsoft.Data.Sqlite.Tests.EntityFramework.BasicEFTests+Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("Microsoft.Data.Sqlite.Tests.EntityFramework.BasicEFTests+Post", b =>
+                {
+                    b.HasOne("Microsoft.Data.Sqlite.Tests.EntityFramework.BasicEFTests+Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
